40a3fa1545ddabe0dab11fe2fcc41ed3c8a54543
jhonochoa <jhon.ochoa@unillanos.edu.co>
2019-07-12 20:23:17 -0500

    Relación en modelo de datos Se realiza el llamado a las funciones creadas en lib y modelos, para poder definir las realciones que hay entre el agente la metrica.

--- nowver-db/index.js
+++ nowver-db/index.js
@@ -1,6 +1,20 @@
 'use strict'
 
+const setupDatabase = require('./lib/db')
+const setupAgent = require('./modelos/agent')
+const setupMetric = require('./modelos/metric')
+
 module.exports = async function (config) {
+  const sequelize = setupDatabase(config);
+  const AgentModel = setupAgent(config)
+  const MetricModel = setupMetric(config)
+
+  /** Se define la relación entre agentes y metricas*/
+  AgentModel.hasMany(MetricModel)
+  MetricModel.belongsTo(AgentModel)
+
+  await sequelize.authenticate()
+
   const Agent = {}
   const Metric = {}
 
