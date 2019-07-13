eb53b9e3f4a7b83952e5abf15ae47e6325095ca1
jhonochoa <jhon.ochoa@unillanos.edu.co>
2019-07-12 21:50:54 -0500

    Prompt de confirmación Se añade prompt de confirmación de eliminación de BD. Si el usuario dice que si, borra y crea la BD de nuevo. En caso de que no NO realiza nada. Ademas se agrega color al mensaje de error para hacerlo mas visible.

--- nowver-db/setup.js
+++ nowver-db/setup.js
@@ -1,13 +1,26 @@
 'use stric'
 
 const debug = require('debug')('nowver:db:setup')
+const inquirer = require('inquirer')
+const chalk = require('chalk')
 const db = require('./')
 
+const prompt = inquirer.createPromptModule()
+
 async function setup() {
+    const answer = await prompt([{
+        type: 'confirm',
+        name: 'setup',
+        message: 'This will destroy your database, are you sure?'
+    }])
+
+    if (!answer.setup) {
+        return console.log('Nothing happened')
+    }
     const config = {
         database: process.env.DB_NAME || 'nowver',
         username: process.env.DB_USER || 'user_db',
-        password: process.env.DB_PASS || '1106',
+        password: process.env.DB_PASS || '110',
         host: process.env.DB_HOST || '192.168.99.100',
         dialect: 'postgres',
         setup: 'true',
@@ -20,7 +33,7 @@ async function setup() {
 }
 
 function handleFatalError(err) {
-    console.error(err.message)
+    console.error(`${chalk.red('[Fatal Error]')} ${err.message}`)
     console.error(err.stack)
     process.exit(1)
 }
