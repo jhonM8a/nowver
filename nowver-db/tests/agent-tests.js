aeb8ef23c27966fecb2a065dfdf2aa4dd45b0738
jhonochoa <jhon.ochoa@unillanos.edu.co>
2019-07-17 20:16:02 -0500

    Se añade stups y mocks con proxiquire y sino Los stups sirven para garantizar que se hagan las llamadas a las funciones adecuadas es decir abstraemos los modelos en este caso osea un mock. Y proxiquire permite sobre escribir llamados.

--- nowver-db/tests/agent-tests.js
+++ nowver-db/tests/agent-tests.js
@@ -1,15 +1,44 @@
 "use stric"
 
 const test = require("ava")
+const proxiquire = require("proxyquire")
+const sinon = require("sinon")
+
 let db = null;
+let sandbox = null
 let config = {
   logging: function () { }
 }
+
+let MetricStub = {
+  belongsTo: sinon.spy()
+}
+
+let AgentStub = null
+
 test.before(async () => {
-  const setupDatabase = require('../')
+  sandbox = sinon.createSandbox()
+  AgentStub = {
+    hasMany: sandbox.spy()
+  }
+  const setupDatabase = proxiquire('../', {
+    './modelos/agent': () => AgentStub,
+    './modelos/metric': () => MetricStub
+  })
   db = await setupDatabase(config)
+})
+
+test.afterEach(() => {
+  sandbox && sinon.resetHistory()
 })
 
 test("Agent", t => {
   t.truthy(db.Agent, 'Agent service exists')
+})
+
+test.serial('Setup', t => {
+  t.true(AgentStub.hasMany.called, 'AgentModel.hasMany has executed')
+  t.true(AgentStub.hasMany.calledWith(MetricStub), 'Argument should be the model')
+  t.true(MetricStub.belongsTo.called, 'MetricModel.belongsTo has executed')
+  t.true(MetricStub.belongsTo.calledWith(AgentStub), 'Argument  should be the model')
 })
