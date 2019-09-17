b6b241884c97a785baf9007b76570ee1cdbea6a5
jhonochoa <jhon.ochoa@unillanos.edu.co>
2019-09-16 20:08:19 -0500

    Implementación de las rutas con el modulo DB

--- nowver-api/api.js
+++ nowver-api/api.js
@@ -6,6 +6,14 @@ const debug = require('debug')('nowver:api:routes')
 const express = require('express')
 const db = require('nowver-db')
 const utils = require('nowver-utils')
+const configTest = {
+    database: process.env.DB_NAME || 'nowver',
+    username: process.env.DB_USER || 'user_db',
+    password: process.env.DB_PASS || '1106',
+    host: process.env.DB_HOST || '192.168.99.100',
+    dialect: 'postgres',
+    port: '5432'
+}
 //Permite dar soporte async y await a middleware y rutas de express
 const asyncify = require('express-asyncify')
 
@@ -17,8 +25,9 @@ const api = asyncify(express.Router())
 api.use('*', async (req, res, next)=>{
     if(!services){
         debug('Connecting to Database')
+        debug(utils.db)
         try {
-            services = await db(utils.db)
+            services = await db(configTest)
         } catch (error) {
             return next(error)
         }
@@ -28,25 +37,58 @@ api.use('*', async (req, res, next)=>{
     next()
 })
 
-api.get('/agents',(req, res)=>{
+api.get('/agents', async (req, res, next)=>{
     debug('A request has come to /agents')
-    res.send({})
-})
-api.get('/agent/:uuid', (req, res, next)=>{
-    const { uuid } = req.params
     
-    if(uuid !== 'yy'){
-        return next(new Error('Agent not found'))
+    let agents = []
+    try {
+        agents = await Agent.findConnected()
+    } catch (error) {
+        return next(error)
+    }
+    res.send(agents)
+})
+api.get('/agent/:uuid', async(req, res, next)=>{
+    const { uuid } = req.params
+    debug(`request to /agent/${uuid}`)
+    let agent
+    try {
+        agent = await Agent.findByUuid(uuid)
+    } catch (error) {
+        next(error)
+    }
+    if(!agent){
+        return next(new Error(`Agent not found with uiid: ${uuid}`))
     }
 
-    res.send({uuid})
+    res.send(agent)
 })
-api.get('/metrics/:uuid', (req, res)=>{
+api.get('/metrics/:uuid', async (req, res, next)=>{
     const { uuid } = req.params
-    res.send({uuid})
+
+    let metrics = []
+    try {
+        metrics = await Metric.findByAgentUuid(uuid)
+    } catch (error) {
+        next(error)
+    }
+    if(!metrics || metrics.length===0){
+        return next(new Error(`Metrics not found for Agent with uiid: ${uuid}`))
+    }
+    res.send(metrics)
 })
-api.get('/metrics/:uuid/:type', (req, res) =>{
+api.get('/metrics/:uuid/:type',async (req, res, next) =>{
     const { uuid, type } = req.params
-    res.send({uuid,type})
+    debug(`request to /metrics/${uuid}/${type}`)
+    let metrics = []
+    try {
+        metrics = await Metric.findByTypeAgentUuid(type, uuid)
+    } catch (error) {
+        next(error)
+    }
+    if(!metrics || metrics.length===0){
+        return next(new Error(`Metrics (${type}) not found for Agent with uiid: ${uuid}`))
+    }
+    res.send(metrics)
 })
 module.exports = api
